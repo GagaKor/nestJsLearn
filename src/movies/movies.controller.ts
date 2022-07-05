ca0343139ecd900f6a93e314f4052a56dd701ce6
KwonHyeokJoo <gagaKor@gmail.com>
2022-07-05 13:35:01 +0900

    DTO 연습

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,44 +1,41 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
-import { Movie } from './entities/Movie.entity';
-import { MoviesService } from './movies.service';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from "@nestjs/common";
+import { CreateMovieDto } from "./dto/create-movie.dto";
+import { UpdateMovieDto } from "./dto/update-movie.dto";
+import { Movie } from "./entities/Movie.entity";
+import { MoviesService } from "./movies.service";
 
-@Controller('movies')
+@Controller("movies")
 export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
 
-    constructor(private readonly moviesService: MoviesService){}
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll(); //"This will return all movies";
+  }
 
-    @Get()
-    getAll() : Movie[]{
-        return this.moviesService.getAll();//"This will return all movies";
-    }
+  @Get("search")
+  search(@Query("year") searchingYear: number) {
+    return `영화 검색 년도 : ${searchingYear}`;
+  }
 
-    @Get("search")
-    search(@Query('year') searchingYear : string){
-        return `영화 검색 년도 : ${searchingYear}`
-    }
-    
-    @Get(':id')
-    getOne(@Param('id') movieId: string):Movie{
-        return  this.moviesService.getOne(movieId) //`This will return one movie with the id ${id}`;
-    }
+  @Get(":id")
+  getOne(@Param("id") movieId: number): Movie {
+    return this.moviesService.getOne(movieId); //`This will return one movie with the id ${id}`;
+  }
 
-    @Post()
-    create(@Body() movieData){
-       return this.moviesService.create(movieData);
-    }
-    
-    @Delete(':id')
-    remove(@Param('id') movieId:string){
-        // return `This will delete a movie with the id ${movieId}`
-        return this.moviesService.deleteOne(movieId);
-    }
+  @Post()
+  create(@Body() movieData: CreateMovieDto) {
+    return this.moviesService.create(movieData);
+  }
 
-    @Patch(':id')
-    path(@Param('id') movieId : string, @Body() updateDate){
-        return {
-            updateMovie : movieId,
-            ...updateDate
-        } //`This will Patch a movie with the id ${movieId}`
-    }
+  @Delete(":id")
+  remove(@Param("id") movieId: number) {
+    // return `This will delete a movie with the id ${movieId}`
+    return this.moviesService.deleteOne(movieId);
+  }
 
+  @Patch(":id")
+  path(@Param("id") movieId: number, @Body() updateDate: UpdateMovieDto) {
+    return this.moviesService.update(movieId, updateDate);
+  }
 }
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,27 +1,39 @@
-import { Injectable } from '@nestjs/common';
-import { Movie } from './entities/Movie.entity';
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { CreateMovieDto } from "./dto/create-movie.dto";
+import { UpdateMovieDto } from "./dto/update-movie.dto";
+import { Movie } from "./entities/Movie.entity";
 
 @Injectable()
 export class MoviesService {
-    private movies:Movie[] = [];
+  private movies: Movie[] = [];
 
-    getAll(): Movie[]{
-        return this.movies;
-    }
+  getAll(): Movie[] {
+    return this.movies;
+  }
 
-    getOne(id:string):Movie{
-        return this.movies.find(movie => movie.id == +id);
+  getOne(id: number): Movie {
+    const movie = this.movies.find(movie => movie.id == +id);
+    if (!movie) {
+      throw new NotFoundException(`해당 영화 '아이디 : ${id}'는 존재하지 않습니다.`);
     }
+    return movie;
+  }
 
-    deleteOne(id:string):boolean{
-        this.movies.filter(movie=>movie.id !== +id);
-        return true;
-    }
+  deleteOne(id: number) {
+    this.getOne(id);
+    this.movies = this.movies.filter(movie => movie.id !== +id);
+  }
 
-    create(movieData){
-        this.movies.push({
-            id: this.movies.length+1,
-            ...movieData
-        })
-    }
+  create(movieData: CreateMovieDto) {
+    this.movies.push({
+      id: this.movies.length + 1,
+      ...movieData,
+    });
+  }
+
+  update(id: number, updateData: UpdateMovieDto) {
+    const movie = this.getOne(id);
+    this.deleteOne(id);
+    this.movies.push({ ...movie, ...updateData });
+  }
 }
