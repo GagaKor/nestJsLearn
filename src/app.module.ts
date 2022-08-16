1bf4d9d1e87987ad91c68df8b231861684cf0c1a
KwonHyeokJoo <gagaKor@gmail.com>
2022-08-16 10:51:17 +0900

    env 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,39 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { MoviesModule } from "./movies/movies.module";
-import { AppController } from "./app.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import * as Joi from "joi";
+
+import { AppController } from "./app.controller";
 import { Movie } from "./movies/entities/Movie.entity";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ".dev.env",
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid("dev", "prod").required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+      }),
+    }),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'testData',
+      type: "mysql",
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [Movie],
       synchronize: true,
-    })
-    ,MoviesModule],
+      logging: true,
+    }),
+    MoviesModule,
+  ],
   controllers: [AppController],
   providers: [],
 })
