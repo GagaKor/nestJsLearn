24cbb6f4e59b4aeaf869fd96d2ef2a5f0a8e2bd2
KwonHyeokJoo <gagaKor@gmail.com>
2022-11-23 10:24:00 +0900

    삭제

--- src/boards/dto/update-Board.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { CreateBaordDto } from "src/boards/dto/create-Board.Dto";
-
-export class UpdateBoardDto extends PartialType(CreateBaordDto) {}
