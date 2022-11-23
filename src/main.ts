f9588c338cee3e89d3d354746ba38ba7427b3773
GagaKor <wexdaq554@gmail.com>
2022-11-24 00:09:38 +0900

    원복

--- src/main.ts
+++ src/main.ts
@@ -17,9 +17,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  if (process.env.NODE_ENV === 'prod') {
-    app.setGlobalPrefix('/api');
-   }
+  
   const port = process.env.PORT || 3000;
   await app.listen(port);
   logger.log(`------------${process.env.NODE_ENV} App Listening at localhost:${port}------------`);
