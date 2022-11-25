c40dff7f6d73083c1c6f7cd2e17320ea39e37edc
KwonHyeokJoo <gagaKor@gmail.com>
2022-11-25 17:16:16 +0900

    크론 테스트

--- src/batch-module/task.service.ts
+++ src/batch-module/task.service.ts
@@ -10,7 +10,7 @@ export class TaskService {
   private readonly logger = new Logger(TaskService.name);
   constructor(private readonly lottoService: LottoService) {}
 
-  @Cron('0 30 * * * *', { name: 'Excel Download' })
+  @Cron('0 20 * * * *', { name: 'Excel Download' })
   async downloadCron() {
     this.logger.log('TASK CALLED DownloadCron');
 
@@ -20,7 +20,7 @@ export class TaskService {
     this.logger.log('Finish Cron Job');
   }
 
-  @Cron('0 35 * * * *', { name: 'Save Lotto' })
+  @Cron('0 25 * * * *', { name: 'Save Lotto' })
   async saveLottoCron() {
     this.logger.log('TASK CALLED SaveLottoCron');
 
