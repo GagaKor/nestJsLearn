a69e0026166458b8fae2f16abfd04d00e21aead0
GagaKor <wexdaq554@gmail.com>
2022-12-10 23:13:54 +0900

    build

--- src/batch-module/task.service.ts
+++ src/batch-module/task.service.ts
@@ -10,7 +10,7 @@ export class TaskService {
   private readonly logger = new Logger(TaskService.name);
   constructor(private readonly lottoService: LottoService) {}
 
-  @Cron('10 23 * * 6', { name: 'Excel Download' })
+  @Cron('17 23 * * 6', { name: 'Excel Download' })
   async downloadCron() {
     this.logger.log('TASK CALLED DownloadCron');
 
@@ -20,7 +20,7 @@ export class TaskService {
     this.logger.log('Finish Cron Job');
   }
 
-  @Cron('13 22 * * 6', { name: 'Save Lotto' })
+  @Cron('20 23 * * 6', { name: 'Save Lotto' })
   async saveLottoCron() {
     this.logger.log('TASK CALLED SaveLottoCron');
 
