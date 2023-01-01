97786eff6c71bd8b9a947f530419d0e7119ec2b2
GagaKor <wexdaq554@gmail.com>
2023-01-01 11:59:58 +0900

    cron 시간변경

--- src/batch-module/task.service.ts
+++ src/batch-module/task.service.ts
@@ -10,7 +10,7 @@ export class TaskService {
   private readonly logger = new Logger(TaskService.name);
   constructor(private readonly lottoService: LottoService) {}
 
-  @Cron('30 21 * * 6', { name: 'Excel Download' })
+  @Cron('0 0 * * 0', { name: 'Excel Download' })
   async downloadCron() {
     this.logger.log('TASK CALLED DownloadCron');
 
@@ -20,7 +20,7 @@ export class TaskService {
     this.logger.log('Finish Cron Job');
   }
 
-  @Cron('32 21 * * 6', { name: 'Save Lotto' })
+  @Cron('0 0 * * 0', { name: 'Save Lotto' })
   async saveLottoCron() {
     this.logger.log('TASK CALLED SaveLottoCron');
 
