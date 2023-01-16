a9cdd7fd0f135a2f66987f666ecd9683bfce6bc2
KwonHyeokJoo <gagaKor@gmail.com>
2023-01-16 13:56:10 +0900

    download test

---
+++ dist/auth/pipe/boardsStatusValidation.pipe.d.ts
@@ -0,0 +1,7 @@
+import { PipeTransform } from '@nestjs/common';
+import { Role } from '../role.enum';
+export declare class RoleValidationPipe implements PipeTransform {
+    readonly StatusOptions: Role[];
+    transform(value: any): any;
+    private isStatus;
+}
