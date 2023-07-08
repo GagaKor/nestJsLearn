a98639aa08b0125b93553cc9989bfe11ae7d9593
GagaKor <wexdaq554@gmail.com>
2023-07-08 14:48:59 +0900

    fox: dto validation

--- src/lotto/dto/purchaseLotto.dts.ts
+++ src/lotto/dto/purchaseLotto.dts.ts
@@ -1,5 +1,10 @@
+import { IsArray, IsString } from 'class-validator';
+
 export class PurchaseLottoDto {
+  @IsArray()
   lottos: number[][];
+  @IsString()
   lottoId: string;
+  @IsString()
   lottoPw: string;
 }
