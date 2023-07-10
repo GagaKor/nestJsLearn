82878482f5394b426cb6b3a4f76cded7285afecc
gagakor <wexdaq554@gmail.com>
2023-07-10 17:11:15 +0900

    원복

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: 'app',
+      script: path.resolve(__dirname, './dist/main.js'),
+      wait_ready: true,
+      instances: 0,
+      exec_mode: 'cluster',
+      listen_timeout: 50000,
+      env: {
+        NODE_ENV: 'prod',
+      },
+    },
+  ],
+};
