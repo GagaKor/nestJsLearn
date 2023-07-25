447e9da4f5b2ea35d48c28f1d95d53d5e5e3f7d6
gagakor <wexdaq554@gmail.com>
2023-07-25 16:42:26 +0900

    lint

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,27 +1,27 @@
-import { Test, TestingModule } from "@nestjs/testing";
-import { INestApplication, ValidationPipe } from "@nestjs/common";
-import { AppModule } from "src/app.module";
-import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
-import { UsersRepository } from "src/auth/users.repository";
-import { typeORMConfig } from "src/configs/typeorm.config";
-import * as request from "supertest";
-import { BoardsRepository } from "./../src/boards/boards.repository";
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { AppModule } from 'src/app.module';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { UsersRepository } from 'src/auth/users.repository';
+import { typeORMConfig } from 'src/configs/typeorm.config';
+import * as request from 'supertest';
+import { BoardsRepository } from './../src/boards/boards.repository';
 
 class MockTypeOrmConfigServer implements TypeOrmOptionsFactory {
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
-      type: "mysql",
-      database: "testData",
+      type: 'mysql',
+      database: 'testData',
       synchronize: true,
       dropSchema: true,
-      entities: ["src/**/*.entity.ts"],
+      entities: ['src/**/*.entity.ts'],
     };
   }
 }
 
 const mockTypeOrmConfigServer = new MockTypeOrmConfigServer();
 
-describe("authController (e2e)", () => {
+describe('authController (e2e)', () => {
   let app: INestApplication;
   let usersRepository: UsersRepository;
 
@@ -47,32 +47,36 @@ describe("authController (e2e)", () => {
   });
 
   afterAll(async () => {
-    const id = await usersRepository.createQueryBuilder("user").select("user.id").getMany();
-    await usersRepository.createQueryBuilder("user").delete().where(id).execute();
+    const id = await usersRepository
+      .createQueryBuilder('user')
+      .select('user.id')
+      .getMany();
+    await usersRepository
+      .createQueryBuilder('user')
+      .delete()
+      .where(id)
+      .execute();
   });
 
-  describe("POST /auth", () => {
-    it("201(OK) 유저 생성", async () => {
-      const username = "test";
-      const password = "test";
-      const res = await request(app.getHttpServer()).post("/auth/signup").send({
+  describe('POST /auth', () => {
+    it('201(OK) 유저 생성', async () => {
+      const username = 'test';
+      const password = 'test';
+      const res = await request(app.getHttpServer()).post('/auth/signup').send({
         username,
         password,
       });
 
       expect(res.status).toBe(201);
     });
-    it("201(OK) 로그인", async () => {
-      const username = "test";
-      const password = "test";
-      const res = await request(app.getHttpServer()).post("/auth/signin").send({
+    it('201(OK) 로그인', async () => {
+      const username = 'test';
+      const password = 'test';
+      const res = await request(app.getHttpServer()).post('/auth/signin').send({
         username,
         password,
       });
       expect(res.status).toBe(201);
     });
-    
-
   });
- 
 });
